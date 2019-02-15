f2d7d6f74214f81b3b7e53bc7d7e67adebabda2e
rizkisadewa <rizkykysadewa@gmail.com>
2019-02-15 20:37:33 +0800

    missing data

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -2,3 +2,8 @@
 
 # Importing Dataset
 dataset = read.csv('Data.csv')
+
+# Taking case of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -12,3 +12,21 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:,:-1].values
 # Create the dependent variable vector
 Y = dataset.iloc[:,3].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) #setting imputer
+imputer = imputer.fit(X[:, 1:3]) # fit the imputer into independent variable X
+X[:, 1:3] = imputer.transform(X[:, 1:3]) # transform a value NaN as per setting above
+'''
+Setting Imputer
+missing_values is the parameter to look for the missing value in column
+strategy is the parameter to set a value from the method, a few method that we can use
+1. mean = then replacing missing values using the mean along the axis, is the same in average
+2. median = then replacing missing values using the median along the axis
+3. most_frequent = then replacing missing values using the most frequent along the axis
+
+axis
+if axis = 0 then input along columns
+if axis = 1 then input along lines
+'''
