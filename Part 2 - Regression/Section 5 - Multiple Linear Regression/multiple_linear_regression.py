b5f03feb8935dec9591b35f668d92352eb867e29
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-07 17:47:57 +0800

    initial Multiple Linear Regression

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 17:04:08 2019
+
+@author: rizky
+"""
+
+# Multiple Linear Regression 
+
+# Data Processing 
+
+# Import the Libraries 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('50_Startups.csv')
+
+# Make a matrix feature of the three independent variables
+X = dataset.iloc[:,:-1].values
+# Create the dependent variable vector
+Y = dataset.iloc[:,4].values
+
+#Encoding Categorical Data 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder() # Make an object of LabelEncoder
+X[:,3] = labelencoder_X.fit_transform(X[:,3]) # Assign variable X to Encoding the data from column number 0 using method fit_transform()
+# Make a dummy encoding since the column contain 3 values
+onehotencoder = OneHotEncoder(categorical_features = [3]) # Make an object of OneHotEncoder, set column number 0 as a dummy encoder
+X = onehotencoder.fit_transform(X).toarray() # Make a dummy Encoding and make it to array
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+'''
+If the column only contain 2 values, we do not have to make a dummy encoding due to the 
+Machine will know it.
+'''
+
+# Splitting the dataset into Trainning set and Testing set
+from sklearn.model_selection import train_test_split # a library for splitting a dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
+
+'''
+the remark will be updated soon
+'''
+
+# Feature Scaling
+"""
+from sklearn.preprocessing import StandardScaler # import class from library
+sc_X = StandardScaler() # an object from the class above
+X_train = sc_X.fit_transform(X_train) # fit the object and transform it 
+X_test = sc_X.transform(X_test) # we only transfer the object as it's already fitted in train set
+""""
+'''
+for the dependent variable vector do not have to be scalled due to the the value represent
+as category with 2 value "yes" or "no"
+'''
+
