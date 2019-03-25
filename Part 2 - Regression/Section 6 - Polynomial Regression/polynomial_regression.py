b389cec8d4755c8394155c858b0d20b0c65a7bf7
rizkisadewa <rizkykysadewa@gmail.com>
2019-03-25 19:35:04 +0800

    Data Preprocessing

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -6,3 +6,37 @@ Created on Fri Mar 22 10:28:44 2019
 """
 
 # Polynomial Regression 
+
+# Import the Libraries 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+
+# Make a matrix feature of the three independent variables
+X = dataset.iloc[:,1:2].values
+# Create the dependent variable vector
+Y = dataset.iloc[:,2].values
+
+# Splitting the dataset into Trainning set and Testing set
+from sklearn.model_selection import train_test_split # a library for splitting a dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
+
+'''
+the remark will be updated soon
+'''
+
+# Feature Scaling
+"""
+from sklearn.preprocessing import StandardScaler # import class from library
+sc_X = StandardScaler() # an object from the class above
+X_train = sc_X.fit_transform(X_train) # fit the object and transform it 
+X_test = sc_X.transform(X_test) # we only transfer the object as it's already fitted in train set
+""""
+'''
+for the dependent variable vector do not have to be scalled due to the the value represent
+as category with 2 value "yes" or "no"
+'''
+
