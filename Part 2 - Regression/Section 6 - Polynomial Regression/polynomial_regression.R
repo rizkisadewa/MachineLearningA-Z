f0776a5e21bcd0f5672e3318f138896c4a0bcaec
rizkisadewa <rizkykysadewa@gmail.com>
2019-04-02 18:45:43 +0800

    Visualizing Linear and Polynomial Regression with R

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -28,5 +28,24 @@ lin_reg = lm(formula = Salary ~.,
 # Fitting Polynomial Regression to the dataset 
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~. , 
               data = dataset)
+
+# Visualizing the Linear Regressiong results 
+library(ggplot2)
+
+ggplot()+
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), colour='red')+
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)), colour='blue')+
+  ggtitle('Truth or Bluff (Linear Regression)')+
+  xlab('Level')+
+  ylab('Salary')
+
+# Visualizing the Polynomial Regression results
+ggplot()+
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), colour='red')+
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)), colour='blue')+
+  ggtitle('Truth or Bluff (Polynomial Regression)')+
+  xlab('Level')+
+  ylab('Salary')
