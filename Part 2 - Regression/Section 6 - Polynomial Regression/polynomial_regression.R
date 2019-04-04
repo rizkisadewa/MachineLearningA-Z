681713db50ee0617ef9d66eda9aab38dfdb4f9c0
rizkisadewa <rizkykysadewa@gmail.com>
2019-04-04 10:59:45 +0800

    predicting a new result with Linear and Polynomial Regression

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -29,6 +29,8 @@ lin_reg = lm(formula = Salary ~.,
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
+dataset$Level5 = dataset$Level^5
+dataset$Level6 = dataset$Level^6
 poly_reg = lm(formula = Salary ~. , 
               data = dataset)
 
@@ -49,3 +51,16 @@ ggplot()+
   ggtitle('Truth or Bluff (Polynomial Regression)')+
   xlab('Level')+
   ylab('Salary')
+
+# Predicting a new Result with Linear Regression 
+Y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial Regression
+Y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3, 
+                                      Level4 = 6.5^4,
+                                      Level5 = 6.5^5,
+                                      Level6 = 6.5^6))
+
+
