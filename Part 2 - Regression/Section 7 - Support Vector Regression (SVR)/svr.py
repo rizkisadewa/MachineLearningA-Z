4204888e98571c4d156e5be899f91145bbab5633
rizkisadewa <rizkykysadewa@gmail.com>
2019-04-15 14:34:10 +0800

    add visualising in higher resolution SVR in Python

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -13,7 +13,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+y = dataset.iloc[:, 2:3].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
@@ -39,5 +39,16 @@ plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Make Visualising in Higher Resolution
+# Visualising the SVR Regression fir higher resolution
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
