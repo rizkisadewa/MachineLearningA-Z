14aaee52a0996efe0efd1063a3a56893b373a11d
rizkisadewa <rizkykysadewa@gmail.com>
2019-05-11 10:45:51 +0800

    SVM with R and Python

---
+++ Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 17:22:19 2019
+
+@author: rizky
+"""
+
+# Support Vector Machine (SVM)
+# Classification Template
+# Data Processing 
+# Import the Libraries 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+# Make a matrix feature of the three independent variables
+X = dataset.iloc[:, [2,3]].values
+# Create the dependent variable vector
+Y = dataset.iloc[:,4].values
+
+# Splitting the dataset into Trainning set and Testing set
+from sklearn.model_selection import train_test_split # a library for splitting a dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.25, random_state = 0)
+
+'''
+the remark will be updated soon
+'''
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler # import class from library
+sc_X = StandardScaler() # an object from the class above
+X_train = sc_X.fit_transform(X_train) # fit the object and transform it 
+X_test = sc_X.transform(X_test) # we only transfer the object as it's already fitted in train set
+
+'''
+for the dependent variable vector do not have to be scalled due to the the value represent
+as category with 2 value "yes" or "no"
+'''
+
+# Fitting Classifier to the Trainning set
+from sklearn.svm import SVC 
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test set results
+Y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
