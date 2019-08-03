cd8f7f23978e5dafa1b22d4cd53815f4bf94fc47
rizkisadewa <rizkykysadewa@gmail.com>
2019-08-03 11:42:54 +0800

    working on Association Rule Learning with Apriori Algorithm in R

--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.R
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.R
@@ -7,3 +7,12 @@ library(arules)
 # create a sparse matrix
 dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
+
+# ploting the most frequent data
+itemFrequencyPlot(dataset, topN = 100)
+
+# Training Apriori on the dataset 
+rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
+
+# Visualizing The Result
+inspect(sort(rules, by = 'lift')[1:10])
