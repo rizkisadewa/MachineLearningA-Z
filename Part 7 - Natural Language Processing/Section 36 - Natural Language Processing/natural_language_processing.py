fd85a102fce22e91101a636059cfe1b914567bd9
rizkisadewa <rizkykysadewa@gmail.com>
2019-09-29 15:29:21 +0800

    still confused how to use this algorithm for another new data

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -35,5 +35,40 @@ for i in range(0, 1000):
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
 
 
+# Classification with Naive Bayes
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# Fitting classifier to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# MAKING OWN PREDICTION
+X_train_str, X_test_str, y_train_str, y_test_str = train_test_split(corpus, y, test_size = 0.20, random_state = 0)
+X_train2 = cv.fit_transform(X_train_str).toarray()
+X_test2 = cv.fit_transform(X_test_str).toarray()
+
+# Fitting classifier to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier2 = GaussianNB()
+classifier2.fit(X_train2, y_train2)
+
+# Predicting the Test set results
+y_pred2 = classifier2.predict(X_test2)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm2 = confusion_matrix(y_test2, y_pred2)
