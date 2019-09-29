98688f22d90fa9c1258fca7e347b8a3fadb69eb9
rizkisadewa <rizkykysadewa@gmail.com>
2019-09-29 15:03:00 +0800

    Create a Bag of words model : create a data which is contain of words that contain word that has been cleaned

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -17,9 +17,23 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
 # Cleaning the texts
 import re
 import nltk
-nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
-review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
-review = review.lower()
-review = review.split()
-review = [word for word in review if not word in set(stopwords.words('english'))]
+from nltk.stem.porter import PorterStemmer
+corpus = []
+
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Create the Bag of words model 
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+
+
