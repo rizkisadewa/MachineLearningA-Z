04b8bc38681461897fd7170d78e1b379bf3659e1
rizkisadewa <rizkykysadewa@gmail.com>
2019-10-15 06:20:24 +0800

    finished Natural Language Processing with R and used Random Forest for Classification

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.R
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.R
@@ -1,13 +1,45 @@
 # Natural Language Processing 
 # Import Data 
-dataset = read.delim('Restaurant_Reviews.tsv', quote='', stringsAsFactors = FALSE)
+dataset_original = read.delim('Restaurant_Reviews.tsv', quote='', stringsAsFactors = FALSE)
 
 # Cleaning the text
 # install.packages('tm')
-install.packages('SnowballC')
+# install.packages('SnowballC')
 library(tm)
 corpus = VCorpus(VectorSource(dataset$Review))
 corpus = tm_map(corpus, content_transformer(tolower))
 corpus = tm_map(corpus, removeNumbers)
 corpus = tm_map(corpus, removePunctuation)
 corpus = tm_map(corpus, removeWords, stopwords())
+corpus = tm_map(corpus, stemDocument)
+corpus = tm_map(corpus, stripWhitespace)
+
+# Create the Bag of Words model
+dtm = DocumentTermMatrix(corpus)
+dtm = removeSparseTerms(dtm, 0.999)
+dataset = as.data.frame(as.matrix(dtm))
+dataset$Liked = dataset_original$Liked # adding the Liked column to dataset from dataset original
+
+# Encoding the target feature as factor
+dataset$Liked = factor(dataset$Liked, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Liked, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Random Forest Classification to the Training set
+# install.packages('randomForest')
+library(randomForest)
+classifier = randomForest(x = training_set[-692],
+                          y = training_set$Liked, 
+                          ntree = 10)
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-692])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 692], y_pred)
