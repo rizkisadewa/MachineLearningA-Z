166ef570a0376618d50d233d07e7c4906eafe115
rizkisadewa <rizkykysadewa@gmail.com>
2019-12-25 06:27:00 +0800

    solving install keras, done

---
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 22:38:12 2019
+
+@author: rizky
+"""
+
+# Artificial Neural Network
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Importing the keras libraries and packages 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the Ann
