7c7968c1da7228e9bd51310f1377f3e00f7abb66
rizkisadewa <rizkykysadewa@gmail.com>
2019-12-25 08:41:40 +0800

    Feature scalling done

--- Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -29,6 +29,18 @@ X = dataset.iloc[:, 3:13].values # we can see only in console by type X and ente
 # Create the dependent variable vector
 Y = dataset.iloc[:, 13].values
 
+#Encoding Categorical Data 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder() # Make an object of LabelEncoder
+X[:,1] = labelencoder_X_1.fit_transform(X[:,1]) 
+labelencoder_X_2 = LabelEncoder() # Make an object of LabelEncoder
+X[:,2] = labelencoder_X_2.fit_transform(X[:,2]) 
+# Make a dummy encoding since the column contain 3 values
+onehotencoder = OneHotEncoder(categorical_features = [1]) # Make an object of OneHotEncoder, set column number 0 as a dummy encoder
+X = onehotencoder.fit_transform(X).toarray() # Make a dummy Encoding and make it to array
+X = X[:, 1:]
+
+
 # Splitting the dataset into Trainning set and Testing set
 from sklearn.model_selection import train_test_split # a library for splitting a dataset
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.25, random_state = 0)
@@ -38,7 +50,6 @@ the remark will be updated soon
 '''
 
 # Feature Scaling
-
 from sklearn.preprocessing import StandardScaler # import class from library
 sc_X = StandardScaler() # an object from the class above
 X_train = sc_X.fit_transform(X_train) # fit the object and transform it 
@@ -55,41 +66,6 @@ as category with 2 value "yes" or "no"
 # Predicting the Test set results
 Y_pred = classifier.predict(X_test)
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, Y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, Y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
 
 # Importing the keras libraries and packages 
 import keras
